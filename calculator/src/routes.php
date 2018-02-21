c64d2f040d5f6cc4d6669ffdb47fea49869a87cd
saurav <skuwar@olivemedia.co>
2018-02-21 15:47:42 +0545

    remanaged

--- calculator/src/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::get('calculator', function(){
-	echo 'Hello from the calculator package!';
-});
-
-Route::get('add/{a}/{b}', 'Saurav\Calculator\CalculatorController@add');
-Route::get('subtract/{a}/{b}', 'Saurav\Calculator\CalculatorController@subtract');
